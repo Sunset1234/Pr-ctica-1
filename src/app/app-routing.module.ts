8953ec5328c22574f712fdb4849cab354726987d
Sunset1234 <emmanuel_burciaga@hotmail.com>
2019-05-30 18:35:50 -0500

    Subiendo proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { PracticaComponent } from './practica/practica.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'practica',component:PracticaComponent},
+
+  {path:'practica/:nombre',component:PracticaComponent},
+
+  //ruta para las que no existen
+
+  {path:'**',component:AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
